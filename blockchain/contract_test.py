6c7f5b6a40cde206205bffd468a929700354b1de
trustytrojan <tarbot24@hotmail.com>
2023-11-28 14:30:20 -0500

    actually looked at the docs

--- blockchain/contract_test.py
+++ blockchain/contract_test.py
@@ -1,25 +1,18 @@
-from typing import Optional
+from typing import Any, Optional
 from web3 import Web3, HTTPProvider
 from web3.types import TxParams
+from web3.contract.contract import Contract
 from json import load
 
 w3 = Web3(HTTPProvider("https://sepolia.infura.io/v3/0776cf37dfb04efdacd478388c7c1dec"))
 
 parse_json_file = lambda filepath: load(open(filepath))
 
-w3stuff = parse_json_file("w3stuff.json")
+w3stuff: dict[str, Any] = parse_json_file("w3stuff.json")
 account: str = w3stuff["account"]
-contract = w3.to_checksum_address(w3stuff["contract"])
-private_key: str = w3stuff["private_key"]
+contract: Contract = w3.eth.contract(w3stuff["contract"], abi=parse_json_file("abi.json"))
 
-contract = w3.eth.contract(address=contract, abi=parse_json_file("abi.json"))
-
-def transact(func_name: str, txparams: Optional[TxParams]):
-	txn = contract.functions.__getattr__(func_name).build_transaction(txparams)
-	signed_txn = w3.eth.account.sign_transaction(txn, private_key)
-	return w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-txn_hash = transact("deposit", {
+txn_hash = contract.functions.deposit().transact({
 	"from": account,
 	"value": w3.to_wei(50, "wei")
 })
